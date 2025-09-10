e9c15ca99b85c254880a90584fc1890384397a9b
Yusuf Abdullah <yusufabdullah20000@gmail.com>
2025-09-10 10:18:10 +0600

    done 2nd excercise

--- Day 1.js
+++ Day 1.js
@@ -1,3 +1,5 @@
+// Exercise 1: Personal Information
+
 const personalInfo=(firstName,lastName,birthYear,currentAge,isStudent)=>{
 let firstname=firstName;
 let lastname=lastName;
@@ -9,4 +11,20 @@ console.log(`First Name: ${firstname}\nLast Name: ${lastname}\nBirth Year: ${bir
 console.log(typeof firstname,typeof lastname,typeof birthyear,typeof currentage,typeof isstudent);  
 }
 
-console.log(personalInfo("Yusuf","Abdullah",1200,24,true))
+// console.log(personalInfo("Yusuf","Abdullah",1200,24,true))
+
+
+// Exercise 2: Data Type Detective
+
+const dataTypeDetective=(value1,value2,value3,value4,value5)=>{
+
+    let myStr=`type of ${value1} = ${typeof value1}
+                typeoof ${value2} = ${typeof value2}
+                typeoof ${value3} = ${typeof value3}
+                typeoof ${value4} = ${typeof value4}
+                typeoof ${value5} = ${typeof value5}
+    `
+    console.log(myStr)
+}
+
+console.log(dataTypeDetective("Hello",42,true,null,undefined));
